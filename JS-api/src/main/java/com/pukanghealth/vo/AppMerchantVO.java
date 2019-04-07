27d9cc11b5c3ff3ec47509acc5f06998760bb1bf
Devil0305 <42987757+Devil0305@users.noreply.github.com>
2019-04-07 16:50:15 +0800

    返回网点类型

---
+++ JS-api/src/main/java/com/pukanghealth/vo/AppMerchantVO.java
@@ -0,0 +1,16 @@
+
+package com.pukanghealth.vo;
+
+import com.pukanghealth.entity.AppMerchantEntity;
+import lombok.Data;
+
+/**
+ * @description
+ * @author Liukang
+ * @create 2019/4/7
+ * @since 1.0.0
+ */
+@Data
+public class AppMerchantVO extends AppMerchantEntity {
+    private String appMerchantTypeName;
+}
---
+++ JS-api/src/main/java/com/pukanghealth/vo/AppNetworkVO.java
@@ -0,0 +1,13 @@
+package com.pukanghealth.vo;
+
+import com.pukanghealth.entity.AppNetworkEntity;
+import lombok.Data;
+/**
+ * @Author: liuKang
+ * @Date: 2019/4/7 16:32
+ */
+
+@Data
+public class AppNetworkVO extends AppNetworkEntity {
+    private String appNetworkTypeName;
+}
