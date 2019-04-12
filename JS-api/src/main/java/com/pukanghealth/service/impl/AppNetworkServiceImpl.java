35cced44977a3a41c1efda9a722519e60565e455
Devil0305 <42987757+Devil0305@users.noreply.github.com>
2019-04-12 09:43:48 +0800

    Update AppNetworkServiceImpl.java

--- JS-api/src/main/java/com/pukanghealth/service/impl/AppNetworkServiceImpl.java
+++ JS-api/src/main/java/com/pukanghealth/service/impl/AppNetworkServiceImpl.java
@@ -36,6 +36,12 @@ public class AppNetworkServiceImpl extends ServiceImpl<AppNetworkDao, AppNetwork
 
     @Override
     public PageUtils queryCCBPage(AppNetworkAO appNetworkAO) {
+        if (appNetworkAO.getCurrPageNo() == 0){
+            appNetworkAO.setCurrPageNo(1);
+        }
+        if (appNetworkAO.getPageSize() == 0){
+            appNetworkAO.setPageSize(10);
+        }
         List<AppNetworkEntity> appNetworkEntityList = appNetworkDao.getCBBNetworkList(appNetworkAO);
         int totalCount = appNetworkDao.getCBBNetworkCount(appNetworkAO);
         return new PageUtils(appNetworkEntityList, totalCount, appNetworkAO.getPageSize(), appNetworkAO.getCurrPageNo());
@@ -43,19 +49,30 @@ public class AppNetworkServiceImpl extends ServiceImpl<AppNetworkDao, AppNetwork
 
     @Override
     public PageUtils querySocialSecurityPage(AppNetworkAO appNetworkAO) {
+        if (appNetworkAO.getCurrPageNo() == 0){
+            appNetworkAO.setCurrPageNo(1);
+        }
+        if (appNetworkAO.getPageSize() == 0){
+            appNetworkAO.setPageSize(10);
+        }
         List<Integer> optionIds;
         List<Integer> moduleIds = new ArrayList<>();
-        if (appNetworkAO.getOptionIds() != null) {
+        if (appNetworkAO.getOptionIds() != null && appNetworkAO.getOptionIds().size() > 0) {
             optionIds = appNetworkAO.getOptionIds();
             for (Integer optionId : optionIds) {
-                List<AppModuleOptionEntity> appModuleOptionEntityList = appModuleOptionDao.selectList(new QueryWrapper<AppModuleOptionEntity>().eq("option_id", optionId));
+                List<AppModuleOptionEntity> appModuleOptionEntityList = appModuleOptionDao.selectList(new QueryWrapper<AppModuleOptionEntity>().eq("option_id", optionId).eq("type", 1));
+                if (null == appModuleOptionEntityList || appModuleOptionEntityList.size() == 0){
+                    return null;
+                }
                 for (AppModuleOptionEntity appModuleOptionEntity : appModuleOptionEntityList) {
                     Integer moduleId = appModuleOptionEntity.getModuleId();
                     moduleIds.add(moduleId);
                 }
             }
         }
-        appNetworkAO.setModuleIds(moduleIds);
+        if (moduleIds.size() > 0){
+            appNetworkAO.setModuleIds(moduleIds);
+        }
         List<AppNetworkVO> appNetworkVOList = new ArrayList<>();
         List<AppNetworkEntity> appNetworkEntityList = appNetworkDao.getSocialSecurityList(appNetworkAO);
         for (AppNetworkEntity appNetworkEntity : appNetworkEntityList) {
@@ -63,10 +80,11 @@ public class AppNetworkServiceImpl extends ServiceImpl<AppNetworkDao, AppNetwork
             BeanUtils.copyProperties(appNetworkEntity, appNetworkVO);
             List<AppModuleOptionEntity> appModuleOptionEntityList = appModuleOptionDao.selectList(new QueryWrapper<AppModuleOptionEntity>().eq("module_id", appNetworkEntity.getNetworkId()).eq("type", 1));
             for (AppModuleOptionEntity appModuleOptionEntity : appModuleOptionEntityList) {
-                AppOptionItemEntity appOptionItemEntity = appOptionItemDao.selectById(appModuleOptionEntity.getOptionId());
-                appNetworkVO.setAppNetworkTypeName(appOptionItemEntity.getOptionDisplayValue());
+                AppOptionItemEntity appOptionItemEntity = appOptionItemDao.selectOne(new QueryWrapper<AppOptionItemEntity>().eq("option_id", appModuleOptionEntity.getOptionId()).eq("option_module_id", 1));
+                if (null != appOptionItemEntity) {
+                    appNetworkVO.setAppNetworkTypeName(appOptionItemEntity.getOptionDisplayValue());
+                }
             }
-
             appNetworkVOList.add(appNetworkVO);
         }
         int totalCount = appNetworkDao.getSocialSecurityCount(appNetworkAO);
